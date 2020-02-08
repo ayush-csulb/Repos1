57f0d62d226996a88fd66da2946ede0a010d48a5
ayush-csulb <ayush.vij@student.csulb.edu>
2020-02-07 18:58:33 -0800

    Commit 2

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const pics = [
-    'imgs/kitty_basket.JPG'
-    'imgs/kitty_bed.JPG'
-    'imgs/kitty_door.jpg'
-    'imgs/kitty_laptop.jpg'
-    'imgs/kitty_sink.JPG'
-    'imgs/kitty_wall.jpg'
-]
+var pics = [
+    "imgs/kitty_basket.JPG",
+    "imgs/kitty_bed.JPG",
+    "imgs/kitty_door.jpg",
+    "imgs/kitty_laptop.jpg",
+    "imgs/kitty_sink.JPG",
+    "imgs/kitty_wall.jpg"
+];
 
-const button = document.querySelector('button')
-const img = document.querySelector('img')
-let index = 0
-button.addEventListener('click'm function() {
-
-    index += 1
-    if(index pics.length){
-        index = 0
+var btn = document.querySelector('button');
+var img = document.querySelector('img');
+var counter = 1;
+btn.addEventListener("click", function() {
+    if(counter === 6){
+        counter = 0;
     }
-    img.src()
-})
+    img.src= pics[counter]
+    counter = counter + 1;
+});
